29afa489236991bff674fda0b0047ab93dabb2d3
Jardeson <jardeson777@gmail.com>
2024-01-23 13:34:01 -0300

    chore: remove ".env.development" in seed

--- src/application/controllers/blood-pressure.controller.ts
+++ src/application/controllers/blood-pressure.controller.ts
@@ -2,50 +2,48 @@ import { Request, Response } from "express";
 import BloodPressureRepository from "../repositories/blood-pressure.repository";
 import { CreateBloodPressureUseCase } from "../../domain/useCases/blood-pressure.use-case";
 
-
 class BloodPressureController {
-    async create(req: Request,  res: Response) {
-        try {
-            const { patientId, systolic, diastolic } = req.body;
-            const repositoryBloodPressure = new BloodPressureRepository();
-            const useCase = new CreateBloodPressureUseCase(repositoryBloodPressure);
-                      
-            if (!patientId) {
-                res.status(400).json({
-                  status: 400,
-                  messenger: "patient id missing",
-                });
-                return;
-            }
-            if (!systolic) {
-                res.status(400).json({
-                  status: 400,
-                  messenger: "systolic missing",
-                });
-                return;
-            }
-            if (!diastolic) {
-                res.status(400).json({
-                  status: 400,
-                  messenger: "diastolic missing",
-                });
-                return;
-            }
+  async create(req: Request, res: Response) {
+    try {
+      const { patientId, systolic, diastolic } = req.body;
+      const repositoryBloodPressure = new BloodPressureRepository();
+      const useCase = new CreateBloodPressureUseCase(repositoryBloodPressure);
 
-            const response = await useCase.execute( {patientId, systolic, diastolic });
+      if (!patientId) {
+        res.status(400).json({
+          status: 400,
+          messenger: "patient id missing",
+        });
+        return;
+      }
+      if (!systolic) {
+        res.status(400).json({
+          status: 400,
+          messenger: "systolic missing",
+        });
+        return;
+      }
+      if (!diastolic) {
+        res.status(400).json({
+          status: 400,
+          messenger: "diastolic missing",
+        });
+        return;
+      }
 
-            res.status(201).json({
-              id: response.id,
-            });
+      const response = await useCase.execute({ patientId, systolic, diastolic });
 
-        } catch (e) {
-            const error = e as { message: string };
-            res.status(400).json({
-              status: 400,
-              message: error.message,
-            });
-        }
+      res.status(201).json({
+        id: response.id,
+      });
+    } catch (e) {
+      const error = e as { message: string };
+      res.status(400).json({
+        status: 400,
+        message: error.message,
+      });
     }
+  }
 }
 
 export default new BloodPressureController();
